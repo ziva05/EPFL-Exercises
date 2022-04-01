477c793cda24550e4588a9d47104a9435e0666b0
ziva05 <zivtav@gmail.com>
2022-04-01 15:32:27 +0200

    EPFL Exercices 8.9 et 8.10

--- Chapter8/JavaScript/script.js
+++ Chapter8/JavaScript/script.js
@@ -13,11 +13,76 @@
 // var message = "The second value now contains kiwis: " + fruits[1];
 // console.log(message);
 
-
+// Add
 // var fruits = ["oranges", "apples", "bananas"];
 // fruits.push("tomatoes");
 // console.log(fruits);
 
-var fruits = ["oranges", "apples", "bananas", "kiwis"];
-fruits.splice(2,1);
-console.log(fruits)
+// Remove
+// var fruits = ["oranges", "apples", "bananas", "kiwis"];
+// fruits.splice(2,1);
+// console.log(fruits)
+
+// var fruits = ["oranges", "apples", "bananas", "kiwis"];
+// fruits.splice(2,2);
+// console.log(fruits);
+
+// Sort
+// var fruits = ["oranges", "apples", "bananas", "kiwis"];
+// fruits.sort();
+// console.log(fruits);
+
+// Reverse
+// var fruits = ["oranges", "apples", "bananas", "kiwis"];
+// fruits.reverse();
+// console.log(fruits);
+
+// Count
+// var fruits = ["oranges", "apples", "bananas", "kiwis"];
+// var fruitsCount = fruits.length;
+// console.log(fruitsCount);
+
+// 8.10 Exercise: Your favorite dishes
+// Create a JavaScript array to store your 3 favorite dish:
+// var favouriteDish = ["bananas", "salads", "soups"];
+// List each of them in the console on their own separate line
+// console.log(favouriteDish[0]);
+// console.log(favouriteDish[1]);
+// console.log(favouriteDish[2]);
+
+// // Display the count of your favorite dishes:
+// var favouriteDish = ["bananas", "salads", "soups"];
+// var dishCount = favouriteDish.length;
+// console.log(dishCount);
+
+// // Add a 4th dish to the array:
+// var favouriteDish = ["bananas", "salads", "soups"];
+// favouriteDish.push("fresh avocades");
+// console.log(favouriteDish);
+
+// // Display the count once more within "I have x favorite dishes":
+// var favouriteDish = ["bananas", "salads", "soups", "fresh avocades"];
+// var dishCount = favouriteDish.length;
+// message = "I have " + dishCount.toString() + " favorite dishes.";
+
+// console.log(dishCount); 
+// console.log(message);
+
+// // Remove the 2nd one:
+// var favouriteDish = ["bananas", "salads", "soups", "fresh avocades"];
+// favouriteDish.splice(2,1);
+// console.log(favouriteDish);
+
+// // Sort them in alphabetical order:
+// var favouriteDish = ["bananas", "salads", "fresh avocades"];
+// favouriteDish.sort();
+// console.log(favouriteDish);
+
+// // Display as an array in the console:
+// var favouriteDish = ["bananas", "salads", "fresh avocades"];
+// console.log(favouriteDish);
+
+// Display the count again:
+// var favouriteDish = ["bananas", "salads", "fresh avocades"];
+// var dishCount = favouriteDish.length;
+// console.log(dishCount);
