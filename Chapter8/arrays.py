ddd9abeb47b56624588f2fc24a7277888fe55309
ziva05 <zivtav@gmail.com>
2022-04-01 11:51:09 +0200

    Add Chapter 8 exercise

---
+++ Chapter8/arrays.py
@@ -0,0 +1,115 @@
+# 08.02  
+# employee_one = "Richard Swift"
+# employee_two = "Marie-Anne Petrie"
+# employee_three = "Cody Lightfoot"
+
+# print(employee_one)
+# print(employee_two)
+# print(employee_three)
+
+# employee_one = "Richard Swift"
+# employee_two = "Marie-Anne Petrie"
+# employee_three = "Cody Lightfoot"
+# employee_four = "Julie Stills"
+
+# print(employee_one)
+# print(employee_two)
+# print(employee_three)
+# print(employee_four)
+
+# employees = ["Richard Swift", "Marie-Anne Petrie", "Cody Lightfoot"]
+# print(employees[2])
+
+# 08.03 Exercice
+# preferable_music = ["Heavens", "The peace", "Baby girl", "Home"]
+# print(preferable_music[0])
+# print(preferable_music[1])
+# print(preferable_music[2])
+# print(preferable_music[3])
+
+# employees = ["Richard Swift", "Marie-Anne Petrie", "Cody Lightfoot"]
+# employees[1] = "Marie-Anne Zimmer"
+# print(employees)
+
+# rick = ["Rick", "Grimes", 38, 180, False]
+# daryl = ["Daryl", "Dixon", 43, 189, True]
+# michonne = ["Michonne", "Anthony", 32, False]
+
+# print(rick[1])
+# print(daryl[1])
+# print(michonne[1])
+
+# employees = ["Richard Swift", "Marie-Anne Petrie", "Cody Lightfoot"]
+# employees.append("Laure Simmons")
+
+# print(employees)
+
+# employees = ["Richard Swift", "Marie-Anne Petrie", "Cody Lightfoot", "Laure Simmons"]
+# employees.pop(2)
+
+# print(employees)
+
+# employees = ["Richard Swift", "Marie-Anne Petrie"]
+# number_of_employees = len(employees)
+# message = "At the beginning, there are " + str(number_of_employees) + " employees."
+# print(message)
+
+# employees.append("Cody Lightfoot")
+# employees.append("Laure Simmons")
+
+# number_of_employees = len(employees)
+# message = "The company grows and now has " + str(number_of_employees) + " employees."
+# print(message)
+
+# employees.pop(1)
+
+# number_of_employees = len(employees)
+# message = "Marie-Anne left the company. There are now " + str(number_of_employees) + " employees."
+# print(message)
+
+# employees = ["Richard Swift", "Marie-Anne Petrie", "Cody Lightfoot", "Laure Simmons"]
+# employees.sort()
+
+# print(employees)
+
+# employees = ["Richard Swift", "Marie-Anne Petrie", "Cody Lightfoot", "Laure Simmons"]
+# employees.reverse()
+
+# print(employees)
+
+# 8.7: Exercise: Manipulating your own array
+# Sort it by alphabetical order:
+# preferable_music = ["Heavens", "The peace", "Baby girl", "Home"]
+# preferable_music.sort()
+# print(preferable_music)
+
+# Display each song in it on its own line in the terminal:
+# 
+
+# Add a 5th song at the end of the array:
+# preferable_music = ["Heavens", "The peace", "Baby girl", "Home"]
+
+# preferable_music.append("Faraway")
+# preferable_music.append("One more night")
+# print(preferable_music)
+
+# # Display how many songs are in the array inside "There are x songs in the array.":
+# preferable_music = ["Heavens", "The peace", "Baby girl", "Home", "Faraway","One more night"]
+
+# number_preferable_music = len(preferable_music)
+# message = "There are " + str(number_preferable_music) + " songs in the array."
+# print(message)
+
+# Remove the 3rd song in the array:
+# preferable_music = ["Heavens", "The peace", "Baby girl", "Home", "Faraway","One more night"]
+
+# preferable_music.pop(3)
+# print(preferable_music)
+
+# Display the content of the array on one line:
+preferable_music = ['Heavens', 'The peace', 'Baby girl', 'Faraway', 'One more night']
+
+# Display the length of the array:
+preferable_music = ['Heavens', 'The peace', 'Baby girl', 'Faraway', 'One more night']
+number_preferable_music = len(preferable_music)
+print(number_preferable_music)
