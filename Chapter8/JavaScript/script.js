30bd1cb3ecf2348d81cdcf1f5078508c93fc8dd0
ziva05 <zivtav@gmail.com>
2022-04-02 10:26:42 +0200

    Chapter 8 all exercices

--- Chapter8/JavaScript/script.js
+++ Chapter8/JavaScript/script.js
@@ -88,37 +88,128 @@
 // console.log(dishCount);
 
 // Create a JavaScript array to store your 3 favorite sports: 
-var favouriteSports = ["swimming", "running", "bikinig"];
+// var favouriteSports = ["swimming", "running", "bikinig"];
 
-// List each of them in the console on their own separate line
-console.log(favouriteSports[0]);
-console.log(favouriteSports[1]);
-console.log(favouriteSports[2]);
+// // List each of them in the console on their own separate line
+// console.log(favouriteSports[0]);
+// console.log(favouriteSports[1]);
+// console.log(favouriteSports[2]);
 
-// Display the count of your favorite sports: 
-var sportsCount = favouriteSports.length;
-console.log(sportsCount);
+// // Display the count of your favorite sports: 
+// var sportsCount = favouriteSports.length;
+// console.log(sportsCount);
 
-// Add a 4th sports to the array:
-favouriteSports.push("climbing");
-console.log(favouriteSports);
+// // Add a 4th sports to the array:
+// favouriteSports.push("climbing");
+// console.log(favouriteSports);
 
-// Display the count once more within "I have x favorite sports":
-var SportsCount = favouriteSports.length;
-message = "I have " + SportsCount.toString() + " favorite sports.";
+// // Display the count once more within "I have x favorite sports":
+// var SportsCount = favouriteSports.length;
+// message = "I have " + SportsCount.toString() + " favorite sports.";
 
-console.log(SportsCount); 
-console.log(message);
+// console.log(SportsCount); 
+// console.log(message);
 
-// Remove the 2nd one:
-favouriteSports.splice(2,1);
-console.log(favouriteSports);
+// // Remove the 2nd one:
+// favouriteSports.splice(2,1);
+// console.log(favouriteSports);
 
-// Sort them in alphabetical order:
-favouriteSports.sort();
-console.log(favouriteSports);
+// // Sort them in alphabetical order:
+// favouriteSports.sort();
+// console.log(favouriteSports);
 
-// Display the count again:
-var sportsCount = favouriteSports.length;
-console.log(sportsCount);
+// // Display the count again:
+// var sportsCount = favouriteSports.length;
+// console.log(sportsCount);
 
+// 8.19 JavaScript loops
+// For loop
+// for (var i = 0; i < 5; i = i + 1){
+//     console.log("Step:" + i.toString());
+// }
+
+// While loop
+// var i = 0
+// while (i < 5){
+//     console.log("Hello " + i.toString());
+//     i = i + 1;
+// }
+
+// 8.20 Ex: Write some JavaScript loops!
+// 1 step
+// 2 steps
+// 3 steps
+// Done!
+
+// for (i = 1; i < 4; i = i + 1){
+//     var step = "";
+//     if (i == 1){
+//         step = " step";
+//     } else {
+//         step = " steps";
+//     }
+//     console.log(i.toString() + step);
+// }
+// console.log("Done!");
+
+
+var i = 1;
+while (i < 4) {
+    var step = "";
+    if (i == 1){
+        step = " step";
+    } else {
+        step = " steps";
+    }
+    console.log(i.toString() + step);
+    i = i + 1;
+}
+
+// 8.25 Looping through arrays in JavaScript
+fruits = ["apples", "oranges", "bananas"];
+ for (i = 0; i < fruits.length; i = i + 1){
+     console.log(fruits[i]);
+ }
+
+
+// 8.26 Exercise: Looping through your favorite dishes
+// Here are my favorite dishes:
+// First dish
+// Second dish
+// Third dish
+// What are your favorite dishes?
+
+// favouriteDish = ["bananas", 
+//                  "salads", 
+//                  "soups"];
+
+// console.log("These are my favourite dishes:");
+// for (i = 0; i < favouriteDish.length; i = i + 1){
+//     console.log(favouriteDish[i]);
+// }
+// console.log("What are yours?");
+
+// 8.31. Objects in JavaScript
+// var city = {name: "Nyon", 
+//             country: "Switzerland", 
+//             population: 11000}
+
+// console.log(city.country);
+// console.log(city["name"]);
+
+// var vehicle = {type: "car", 
+//                color: "black",
+//                year: 2019}
+
+// console.log(vehicle.type);
+// console.log(vehicle["color"]);
+
+// 8.32. Exercise: Convert your programmer business card to JavaScript
+var business_card = {"name": "Kevin", 
+                     "surname": "Chappuis",
+                     "adress": "ch. des Alpages 9",
+                     "city": "Lausanne"}
+
+console.log(business_card.name);
+console.log(business_card.surname);
+console.log(business_card.adress);
---
+++ Chapter8/loops.py
@@ -0,0 +1,97 @@
+# 8.12 Again and again:Looping
+# print("Hello!")
+# print("Hello!")
+# print("Hello!")
+# print("Hello!")
+# print("Hello!")
+# print("Hello!")
+# print("Hello!")
+# print("Hello!")
+# print("Hello!")
+# print("Hello!")
+
+# 8.13 Write loops in Python
+# for x in range(1000):
+#     print("Hello!")
+
+# for x in range(10):
+#     print(x)
+
+# for x in range(4):
+#     print("Number: " + str(x))
+
+# for i in range(5):
+#     current_step = str(i + 1)
+#     step_word = ""
+#     if current_step == "1":
+#         step_word = " step "
+#     else:
+#         step_word = " steps "
+#     print("It's " + current_step + step_word)
+
+# 8.14 Exercise: Your first loop
+# for i in range(6):
+#     print(str(i) + " is a number")
+
+# 8.16 Loop while...
+# my_number = 0
+
+# while my_number < 10:
+#     print(my_number)
+#     my_number = my_number + 1 
+
+# i = 0
+
+# while i < 5:
+#     print("Hello!")
+#     i = i + 1
+
+# iterator = 3
+
+# while iterator < 50:
+#     print("Hello!")
+#     iterator = iterator + 13
+
+
+# # 8.22 Looping through an array:
+# fruits = ["apples", "oranges", "bananas", "pears"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+# 8.23 Exercise: Loop through your favorite songs
+# songs = ['Heavens', 
+#         'The peace', 
+#         'Baby girl', 
+#         'One more night']
+
+# print("These are my fovourite songs:")
+# for song in songs:
+#     print(song)
+# print("What are yours?")
+
+# 8.28. Named collections
+# person = ["Aleqa", "Lyberth", "Nuuk", "Greenland"]
+
+# person = {"firstname": "Aleqa",
+#           "lastname": "Lyberth",
+#           "city": "Nuuk",
+#           "country": "Greenland"}
+
+# print(person["city"])
+
+# town = {"name": "Courroux", 
+#          "elevation": 418, 
+#          "population": 3200}
+
+# print(town["name"])
+# print(town["population"])
+
+# 8.29 Exercise: Your programmer business card
+business_card = {"name": "Kevin", 
+                "surname": "Chappuis",
+                "adress": "ch. des Alpages 9",
+                "city": "Lausanne"}
+print(business_card["name"])
+print(business_card["adress"])
+print(business_card["city"])
