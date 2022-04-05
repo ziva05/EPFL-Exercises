4c348d268622a2f4c5e6ba6ad9038d2e90287dde
ziva05 <zivtav@gmail.com>
2022-04-05 12:23:26 +0200

    All commited

---
+++ Chapter10/JavaScript/script.js
@@ -0,0 +1,14 @@
+// Find at least 3 conversion formulas you want to use with your multiconverter
+// Write a function for each formula
+// Use the window.prompt() function to get inputs from the user 
+// Display the conversion results in the browser console
+// Quit when user finished converting the values 
+// If the user chooses a non supported conversion, display that in the console
+
+// Conversion EUR to CHF
+
+
+// Conversion KM to Miles
+
+
+// Conversion xxxx 
